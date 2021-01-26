1238373cb6998cd16cd85c8c601afa0d26be4d03
NickRylander <nick.rylander@outlook.com>
2021-01-26 11:55:07 -0800

    app setup and descriptions

--- src/App.js
+++ src/App.js
@@ -1,25 +1,29 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react';
+import { BrowserRouter as Router, Switch, Route, BrowserRouter } from 'react-router-dom'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+
+  render() {
+    return (
+      <>
+      <Router>
+      {/* <Logo /> */}
+      <br/>
+      {/* <Navbar /> */}
+        <div className="container">
+          <Switch>
+            {/* <Route exact path="/" component={ Home } />
+            <Route exact path="/forecast" component={ Forecast } />
+            <Route exact path="/favorite" component={ Favorites } /> */}
+          </Switch>
+        </div>
+      </Router>
+      </>
+    );
+  }
 }
 
 export default App;
+
+// BrowserRouter: Keeps UI in sync with the URL
+// Switch: Renders the first child <Route> that matches the location. Unique in that is renders a route excusively.In contrast, Route matches the location and renders it inclusively
